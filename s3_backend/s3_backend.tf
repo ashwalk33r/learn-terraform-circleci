922f022612ec1e2cb1333d1367aa3618f514789a
Ash <ashwalk33r@gmail.com>
2021-03-28 13:18:42 +0200

    feat: disable versioning

--- s3_backend/s3_backend.tf
+++ s3_backend/s3_backend.tf
@@ -10,11 +10,9 @@ resource "random_uuid" "randomid" {}
 
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "${random_uuid.randomid.result}-backend"
-  # Enable versioning so we can see the full revision history of our
-  # state files
   force_destroy = true
   versioning {
-    enabled = true
+    enabled = false
   }
   # Enable server-side encryption by default
   server_side_encryption_configuration {
